f90891db8847cb5bccbd1ba7ca35276f039a03d3
Teamsyy <93755080+Teamsyy@users.noreply.github.com>
2023-04-19 11:08:18 +0700

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,10 @@
+package bytestream;
+public class Circle {
+    public class Circle implements  Shape{
+    @Override
+    public void draw(){
+        System.out.println("shape:circle");
+
+}
+
+}
